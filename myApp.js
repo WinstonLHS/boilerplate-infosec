c8707631682bc24a516c371b1c927dd198021779
WinsByte48 <winstonlimhs@gmail.com>
2022-06-13 23:27:56 +0800

    fix helmet middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
-
 
 
 
@@ -50,6 +50,7 @@ const app = express();
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
+app.use(helmet.hidePoweredBy());
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
