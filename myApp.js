b0cabd7bfe2b85506aeac45a76451947c220ce87
WinsByte48 <winstonlimhs@gmail.com>
2022-06-13 23:40:26 +0800

    add prettier and apply prettier rules

--- myApp.js
+++ myApp.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 const app = express();
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
+const api = require("./server.js");
+app.use(express.static("public"));
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'DENY'}))
-app.use(helmet.xssFilter())
-app.disable('strict-transport-security');
-app.use('/_api', api);
+app.use(helmet.frameguard({ action: "DENY" }));
+app.use(helmet.xssFilter());
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
